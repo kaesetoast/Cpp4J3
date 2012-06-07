83fc0ea42f32b42df829be61c1618b31e922067f
kaesetoast <philipp@nowinski.de>
2012-06-07 15:34:16 +0200

    Tree interface

--- Tree.h
+++ Tree.h
@@ -1,9 +1,21 @@
 #ifndef TREE_H
 #define TREE_H
 #include "TreeNode.h"
+#include "TreeIterator.h"
 namespace mystl {
+    typedef TreeIterator<T, O> iterator;
+    typedef TreeNode<T, O> node;
+
     template<typename T, typename O>
     class Tree {
+    public:
+        iterator insert(const T& value);
+        void clear();
+        iterator begin();
+        iterator end();
+        iterator first();
+        iterator last();
+        iterator find(const T& value);
 
     private:
         TreeNode<T, O> m_root;
