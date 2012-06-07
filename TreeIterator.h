f0e5ad0c3f87366d948d176000212fe3f734b677
kaesetoast <philipp@nowinski.de>
2012-06-07 15:30:16 +0200

    TreeIterator interface

--- TreeIterator.h
+++ TreeIterator.h
@@ -5,6 +5,19 @@
 namespace mystl {
     template<typename T, typename O>
     class TreeIterator {
+    public:
+        TreeIterator(TreeNode<T, O> node)
+            : m_node(node){
+
+        }
+
+        T& operator*();
+        T* operator->();
+        TreeIterator<T, O>& operator++();
+        TreeIterator<T, O>& operator--();
+        bool operator==(const TreeIterator<T, O>&);
+        bool operator!=(const TreeIterator<T, O>&);
+
     private:
         Tree<T, O> m_tree;
         TreeNode<T, O> m_node;
