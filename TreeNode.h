eddee7f6e2be714b831c0b29947eb8085d588c79
kaesetoast <philipp@nowinski.de>
2012-06-07 15:25:10 +0200

    TreeNode interface

--- TreeNode.h
+++ TreeNode.h
@@ -4,6 +4,11 @@
 namespace mystl {
     template<typename T, typename O>
     class TreeNode {
+    public:
+        T& value();
+        TreeNode<T, O>* find(const T&);
+        TreeNode<T, O>* findFirst();
+        TreeNode<T, O>* findLast();
     private:
         Tree<T, O> m_left, m_right;
         TreeNode<T, O> m_up;
