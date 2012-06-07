eea3cb7106381b537962de8d4df1e111917ebf28
kaesetoast <philipp@nowinski.de>
2012-06-07 15:36:29 +0200

    friend classes

--- TreeNode.h
+++ TreeNode.h
@@ -4,12 +4,14 @@
 namespace mystl {
     template<typename T, typename O>
     class TreeNode {
+        friend class TreeIterator<T, O>;
+        friend class Tree<T, O>;
     public:
         T& value();
         TreeNode<T, O>* find(const T&);
         TreeNode<T, O>* findFirst();
         TreeNode<T, O>* findLast();
-    private:
+    protected:
         Tree<T, O> m_left, m_right;
         TreeNode<T, O> m_up;
     };
