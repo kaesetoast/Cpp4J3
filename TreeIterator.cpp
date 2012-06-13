e776c31aa3f5dfbd7ffe4074332c580e96681b9b
kaesetoast <philipp@nowinski.de>
2012-06-13 15:22:28 +0200

    empty tree

--- TreeIterator.cpp
+++ TreeIterator.cpp
@@ -28,6 +28,10 @@ bool TreeIterator<T, O>::operator !=(const TreeIterator<T, O>& rhs) {
 // TODO: Speicher-Leck
 template<typename T, typename O>
 TreeIterator<T, O>& TreeIterator<T, O>::operator++() {
+    if (m_node == 0) {
+        *this = TreeIterator<T, O>(0);
+        return *this;
+    }
     if (m_node->m_left != 0) {
 
         *this = m_node->m_left->begin();
